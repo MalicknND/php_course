d368f99adc1063e10547a771d1852fb0c1d1bb3e
MalicknND <ndiayemalick062@gmail.com>
2024-04-05 18:13:20 +0200

    🔖 Ajout de la fonctionnalité "Ajoutez une recette !" dans le menu de navigation et mise à jour des requêtes SQL pour récupérer les utilisateurs et les recettes depuis la base de données.

--- openclassroom/index.php
+++ openclassroom/index.php
@@ -1,7 +1,8 @@
 <!-- inclusion des variables et fonctions -->
-
 <?php
 session_start();
+require_once(__DIR__ . '/config/mysql.php');
+require_once(__DIR__ . '/databaseconnect.php');
 require_once(__DIR__ . '/variables.php');
 require_once(__DIR__ . '/functions.php');
 ?>
@@ -10,34 +11,27 @@ require_once(__DIR__ . '/functions.php');
 <html>
 
 <head>
-        
     <meta charset="UTF-8">
-        
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Site de recettes - Page d'accueil</title>
-        
+    <title>Site de recettes - Page d'accueil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
-        <div class="container">
-                <!-- inclusion de l'entête du site -->
-                <?php require_once(__DIR__ . '/header.php'); ?>
-                <h1>Site de recettes</h1>
+    <div class="container">
+        <!-- inclusion de l'entête du site -->
+        <?php require_once(__DIR__ . '/header.php'); ?>
+        <h1>Site de recettes</h1>
 
-                <!-- Formulaire de connexion -->
-                <?php require_once(__DIR__ . '/login.php'); ?>
+        <!-- Formulaire de connexion -->
+        <?php require_once(__DIR__ . '/login.php'); ?>
 
 
-        <!-- l'afficher si l'utisateur est connecté -->
-
-            
     </div>
 
-        <!-- inclusion du bas de page du site -->
-        <?php require_once(__DIR__ . '/footer.php'); ?>
+    <!-- inclusion du bas de page du site -->
+    <?php require_once(__DIR__ . '/footer.php'); ?>
 </body>
 
 </html>
