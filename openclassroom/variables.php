53d772d6079a0701e881964ecbfbf1dbcd4d92d5
MalicknND <ndiayemalick062@gmail.com>
2024-04-05 16:22:50 +0200

    🔐 cours openclassroom

---
+++ openclassroom/variables.php
@@ -0,0 +1,48 @@
+<?php
+$users = [
+    [
+        'full_name' => 'Malick Ndiaye',
+        'email' => 'ndiaye@exemple.com',
+        'age' => 34,
+        'password' => 'Malick',
+    ],
+    [
+        'full_name' => 'Mathieu Nebra',
+        'email' => 'mathieu.nebra@exemple.com',
+        'age' => 34,
+        'password' => 'Malick',
+    ],
+    [
+        'full_name' => 'Laurène Castor',
+        'email' => 'laurene.castor@exemple.com',
+        'age' => 28,
+        'password' => 'Malick',
+    ],
+];
+
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => 'Etape 1 : des flageolets !',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => 'Etape 1 : de la semoule',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : prenez une belle escalope',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Salade Romaine',
+        'recipe' => 'Etape 1 : prenez une belle salade',
+        'author' => 'laurene.castor@exemple.com',
+        'is_enabled' => false,
+    ],
+];
