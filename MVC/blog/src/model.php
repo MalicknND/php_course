223a6523daa114cd3049e7caa1c7f1941d038667
MalicknND <ndiayemalick062@gmail.com>
2024-04-08 23:10:06 +0200

    ✅

--- MVC/blog/src/model.php
+++ MVC/blog/src/model.php
@@ -1,5 +1,4 @@
 <?php
-// src/model.php
 
 function getPosts()
 {
@@ -35,43 +34,15 @@ function getPost($identifier)
         'title' => $row['title'],
         'french_creation_date' => $row['french_creation_date'],
         'content' => $row['content'],
+        'identifier' => $row['id'],
     ];
 
     return $post;
 }
 
-
-
-function getComments($identifier)
-{
-    $database = dbConnect();
-    $statement = $database->prepare(
-        "SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
-    );
-    $statement->execute([$identifier]);
-
-    $comments = [];
-    while (($row = $statement->fetch())) {
-        $comment = [
-            'author' => $row['author'],
-            'french_creation_date' => $row['french_creation_date'],
-            'comment' => $row['comment'],
-        ];
-
-        $comments[] = $comment;
-    }
-
-    return $comments;
-}
-
-// Nouvelle fonction qui nous permet d'éviter de répéter du code
 function dbConnect()
 {
-    try {
-        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+    $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
 
-        return $database;
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
+    return $database;
 }
